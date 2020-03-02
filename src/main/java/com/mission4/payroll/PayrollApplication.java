890059443019268191fc43185ac15f652283689c
Cheah How Ong <cheah.how.ong@accenture.com>
2020-03-02 18:08:57 +0800

    added log4j2 to employee controller

--- src/main/java/com/mission4/payroll/PayrollApplication.java
+++ src/main/java/com/mission4/payroll/PayrollApplication.java
@@ -1,9 +1,13 @@
 package com.mission4.payroll;
 
+import lombok.extern.log4j.Log4j2;
+import org.hibernate.annotations.common.util.impl.Log;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class PayrollApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PayrollApplication.class, args);
--- src/main/java/com/mission4/payroll/controller/EmployeeController.java
+++ src/main/java/com/mission4/payroll/controller/EmployeeController.java
@@ -2,14 +2,19 @@ package com.mission4.payroll.controller;
 
 import com.mission4.payroll.model.request.CreateEmployeeRequest;
 import com.mission4.payroll.model.request.UpdateEmployeeRequest;
-import com.mission4.payroll.model.response.*;
+import com.mission4.payroll.model.response.CreateEmployeeResponse;
+import com.mission4.payroll.model.response.GetAllEmployeeResponse;
+import com.mission4.payroll.model.response.GetEmployeeResponse;
+import com.mission4.payroll.model.response.UpdateEmployeeResponse;
 import com.mission4.payroll.service.*;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URISyntaxException;
 
 @RestController
+@Log4j2
 public class EmployeeController {
     private GetEmployeeDetailsService getEmployeeDetailsService;
     private GetAllEmployeeDetailsService getAllEmployeeDetailsService;
@@ -31,29 +36,41 @@ public class EmployeeController {
 
     // Aggregate root
     @GetMapping("/employees")
-    public GetAllEmployeeResponse all() {
+    public GetAllEmployeeResponse all(@RequestHeader(name = "userId", required = true) String userId) {
+        log.warn("testing testing");
+        log.debug(userId);
         return getAllEmployeeDetailsService.execute();
     }
 
     @PostMapping("/employees")
-    public CreateEmployeeResponse newEmployee(@RequestBody CreateEmployeeRequest createEmployeeRequest) throws URISyntaxException {
+    public CreateEmployeeResponse newEmployee(@RequestHeader(name = "userId", required = true) String userId, @RequestBody CreateEmployeeRequest createEmployeeRequest) throws URISyntaxException {
+        log.warn("testing testing");
+        log.debug(userId);
         return createEmployeeService.execute(createEmployeeRequest);
     }
 
     // Single item
     @GetMapping("/employees/{id}")
-    public GetEmployeeResponse one(@PathVariable Long id) {
+    public GetEmployeeResponse one(@RequestHeader(name = "userId", required = true) String userId, @PathVariable Long id) {
+        log.warn("testing testing");
+        log.debug(userId);
         return getEmployeeDetailsService.execute(id);
     }
 
     @PutMapping("/employees/{id}")
-    public UpdateEmployeeResponse replaceEmployee(@RequestBody UpdateEmployeeRequest updateEmployeeRequest,
+    public UpdateEmployeeResponse replaceEmployee(@RequestHeader(name = "userId", required = true) String userId,
+                                                  @RequestBody UpdateEmployeeRequest updateEmployeeRequest,
                                                   @PathVariable Long id) throws URISyntaxException {
+        log.warn("testing testing");
+        log.debug(userId);
         return updateEmployeeService.execute(updateEmployeeRequest, id);
     }
 
     @DeleteMapping("/employees/{id}")
-    public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
+    public ResponseEntity<String> deleteEmployee(@RequestHeader(name = "userId", required = true) String userId,
+                                                 @PathVariable Long id) {
+        log.warn("testing testing");
+        log.debug(userId);
         return deleteEmployeeService.execute(id);
     }
 }
